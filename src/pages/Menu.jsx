77f0e1590f552845bf0e9a412cc21c18669e23c0
DanielRodriguezZara <danielrodrizarate@gmail.com>
2025-08-01 22:10:17 -0600

    Fix menu responsive

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -168,60 +168,68 @@ export default function Menu() {
 
   return (
     <>
-      <div className="w-full h-full flex">
-        <div className="w-3/4 h-full flex flex-col gap-6 ">
-          <div className="h-1/2 flex gap-10 items-center justify-center ">
+      <div className="w-full h-full flex flex-col md:flex-row">
+        <div className="w-full md:w-3/4 h-full flex flex-col gap-6">
+          <div className="flex flex-col md:flex-row gap-4 md:gap-10 items-center justify-center py-4">
             <img
               src={menu1}
-              className="w-1/3"
+              className="w-1/2 md:w-1/3"
               style={{ filter: "drop-shadow(1px 1px 10px black)" }}
             />
-            <div>
-              <h1 className="text-lg text-gray-600">#1 Most loved dish</h1>
-              <p className="text-7xl font-light">
-                Vegan <h1 className="font-bold">Salad</h1>
+            <div className="text-center md:text-left">
+              <h1 className="text-base md:text-lg text-gray-600">
+                #1 Most loved dish
+              </h1>
+              <p className="text-4xl md:text-7xl font-light">
+                Vegan{" "}
+                <span className="font-bold sm:inline-block	">
+                  <h1>Salad</h1>
+                </span>
               </p>
             </div>
           </div>
-          <div className="h-1/2 w-full  flex justify-center items-center">
-            {/* Slider */}
+
+          <div className="flex justify-center items-center px-4 md:px-0 pb-4">
             <Swiper
-              slidesPerView={4}
+              slidesPerView={2}
               spaceBetween={5}
               centeredSlides={false}
               navigation
               modules={[Navigation, Pagination]}
-              className=" !pl-24 rounded-lg"
+              breakpoints={{
+                640: { slidesPerView: 3 },
+                1024: { slidesPerView: 4 },
+              }}
+              className="!pl-2 md:!pl-24 rounded-lg"
             >
-              <div className="">
-                {dataPrueba.map((plato, index) => (
-                  <SwiperSlide key={index} className=" ">
-                    <button
-                      onClick={() => onChangeActive(index)}
-                      className={`w-32 h-48  p-4 flex flex-col gap-2 items-center justify-center ${
-                        isActive == index
-                          ? "bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 shadow-md rounded-3xl border border-white"
-                          : null
-                      }`}
-                    >
-                      <img
-                        src={plato.url_imagen}
-                        alt={plato.nombre}
-                        className=" h-[55%] object-fill	"
-                        style={{ filter: "drop-shadow(1px 1px 2px black)" }}
-                      />
-                      <p className="text-sm h-[45%] font-bold text-center">
-                        {plato.nombre}
-                      </p>
-                    </button>
-                  </SwiperSlide>
-                ))}
-              </div>
+              {dataPrueba.map((plato, index) => (
+                <SwiperSlide key={index}>
+                  <button
+                    onClick={() => onChangeActive(index)}
+                    className={`w-full md:w-32 h-48 p-2 md:p-4 flex flex-col gap-2 items-center justify-center ${
+                      isActive === index
+                        ? "bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 shadow-md rounded-3xl border border-white"
+                        : ""
+                    }`}
+                  >
+                    <img
+                      src={plato.url_imagen}
+                      alt={plato.nombre}
+                      className="h-[55%] object-contain"
+                      style={{ filter: "drop-shadow(1px 1px 2px black)" }}
+                    />
+                    <p className="text-sm h-[45%] font-bold text-center">
+                      {plato.nombre}
+                    </p>
+                  </button>
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
-        <div className="w-1/4 h-full flex flex-col justify-between py-10 px-4">
-          <div className="w-full flex justify-center gap-12">
+
+        <div className="w-full md:w-1/4 h-full flex flex-col justify-between py-6 px-4">
+          <div className="w-full flex justify-center gap-12 mb-4">
             <button>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -246,51 +254,42 @@ export default function Menu() {
             </button>
           </div>
 
-          <div className="h-4/5 bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 shadow-md rounded-3xl border border-white p-8">
+          <div className="h-auto md:h-4/5 bg-gray-100 backdrop-filter backdrop-blur-sm bg-opacity-70 shadow-md rounded-3xl border border-white p-6 flex flex-col gap-3">
             <div className="flex justify-between">
-              <button className="font-thin">
-                <h1>Overview</h1>
-              </button>
-              <button className="font-thin text-gray-400">
-                <h1>Ingredients</h1>
-              </button>
+              <button className="font-black">Overview</button>
+              <button className="text-gray-400">Ingredients</button>
             </div>
-            <div className="flex flex-col gap-3 justify-between">
-              <div class="flex flex-col justify-end  relative rounded-xl  py-2 w-[45px] h-24 ">
-                <h1 class="text-gray-500 absolute font-bold text-3xl top-1/2 -translate-y-2/3 right-[-20px]">
-                  5.0
-                </h1>
-                <div className="bg-orange-400 h-[80%] rounded-xl flex items-end justify-center pb-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="18px"
-                    viewBox="0 0 24 24"
-                    width="18px"
-                    fill="#000000"
-                  >
-                    <path
-                      d="M12 17.27L18.18 21 16.54 13.97 
-                        22 9.24l-7.19-.62L12 2 
-                        9.19 8.62 2 9.24 
-                        7.46 13.97 5.82 21z"
-                    />
-                  </svg>
+            <div className="flex flex-col justify-between gap-3 h-full">
+              <div className="flex flex-col justify-center md:justify-start gap-4 items-center md:items-start">
+                <div className="flex flex-col justify-end relative rounded-xl py-2 w-[45px] h-24">
+                  <h1 className="text-gray-500 absolute font-bold text-3xl top-1/2 -translate-y-2/3 right-[-20px]">
+                    5.0
+                  </h1>
+                  <div className="bg-orange-400 h-[80%] rounded-xl flex items-end justify-center pb-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="18px"
+                      viewBox="0 0 24 24"
+                      width="18px"
+                      fill="#000000"
+                    >
+                      <path d="M12 17.27L18.18 21 16.54 13.97 22 9.24l-7.19-.62L12 2 9.19 8.62 2 9.24 7.46 13.97 5.82 21z" />
+                    </svg>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <div>
+                <div className="">
                   <h1 className="text-xl">Gordon Ramsay</h1>
                   <p>British chef</p>
                 </div>
-                <div className="mt-3">
-                  <h6 className="text-gray-500 font-thin text-xs">
-                    If you want to become a great chef you have to work with
-                    great chefs. And that's exactly what I did.
-                  </h6>
-                </div>
               </div>
-              <div className="mt-3 flex w-full justify-between">
-                <button className="flex items-center justify-center gap-2 p-2 bg-gray-300 rounded-xl text-sm">
+
+              <h6 className="text-gray-500 font-thin text-xs mt-2">
+                If you want to become a great chef you have to work with great
+                chefs. And that's exactly what I did.
+              </h6>
+
+              <div className="mt-3 flex w-full justify-between gap-2">
+                <button className="flex-1 flex items-center justify-center gap-2 p-2 bg-gray-300 rounded-xl text-sm">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="15px"
@@ -299,7 +298,7 @@ export default function Menu() {
                     fill="#000000"
                   >
                     <path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z" />
-                  </svg>{" "}
+                  </svg>
                   54 Likes
                 </button>
                 <button className="bg-gray-300 p-2 rounded-lg">
@@ -308,7 +307,7 @@ export default function Menu() {
                     height="15px"
                     viewBox="0 -960 960 960"
                     width="15px"
-                    fill="#ffffff"
+                    fill="#000000"
                   >
                     <path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z" />
                   </svg>
